a1e5bd16e1302905e16a3d5598469d3690df5826
dengri12 <dengri12@gmail.com>
2016-06-02 11:46:08 +0300

    flexbox test starter template

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
-$(document).ready(function() {
-
+/*$(document).ready(function() {
+    
+    console.log('Hello World');
 
 });
+*/
+/*
+$(function(){
+    console.log('Hello from jquery onload short');
+});*/
 
+window.onload = function (params) {
+    
+  /*  document.getElementsByTagName('body')[0].innerHTML = 'Hello World!';*/
+
+}
